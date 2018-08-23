0058a6e5c3ad0ade7edde955e7d64f3fe69b2e42
Justin F <forsbergjj@gmail.com>
2018-08-23 18:48:55 -0400

    Refactor account type enum

--- src/main/java/org/iron/ultimate/model/enums/AccountType.java
+++ src/main/java/org/iron/ultimate/model/enums/AccountType.java
@@ -1,27 +1,20 @@
 package org.iron.ultimate.model.enums;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public enum AccountType {
 
-	REGULAR,
-	IRONMAN,
-	HARDCORE,
-	ULTIMATE;
+	REGULAR(new AccountType[] {}),
+	IRONMAN(new AccountType[] { REGULAR }),
+	HARDCORE(new AccountType[] { IRONMAN, REGULAR }),
+	ULTIMATE(new AccountType[] { IRONMAN, REGULAR });
 
-	private static Map<AccountType, AccountType[]> inheritedAccountTypes;
-
-	static {
-		inheritedAccountTypes = new HashMap<AccountType, AccountType[]>();
-		inheritedAccountTypes.put(REGULAR, new AccountType[] {});
-		inheritedAccountTypes.put(IRONMAN, new AccountType[] { REGULAR });
-		inheritedAccountTypes.put(HARDCORE, new AccountType[] { IRONMAN, REGULAR });
-		inheritedAccountTypes.put(ULTIMATE, new AccountType[] { IRONMAN, REGULAR });
+	private AccountType[] inheritedAccountTypes;
+	
+	private AccountType(AccountType[] inheritedAccountTypes) {
+		this.inheritedAccountTypes = inheritedAccountTypes;
 	}
 	
 	public AccountType[] getInheritedAccountTypes() {
-		return inheritedAccountTypes.get(this);
+		return inheritedAccountTypes;
 	}
 
 }
